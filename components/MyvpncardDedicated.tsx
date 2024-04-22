f6f5eeff6c1127771aeb4690001b1adcace497e3
Punarv Pawade <punarvpawade17@gmail.com>
2024-04-22 09:59:05 +0530

    vpnCard: redirect to auth url

--- components/MyvpncardDedicated.tsx
+++ components/MyvpncardDedicated.tsx
@@ -1,12 +1,10 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import Cookies from "js-cookie";
-import eye from '../public/ph_eye.png';
-import dlt from '../public/dlt.png';
-import {
-  FaCopy,
-} from "react-icons/fa";
-import Image from 'next/image';
+import eye from "../public/ph_eye.png";
+import dlt from "../public/dlt.png";
+import { FaCopy } from "react-icons/fa";
+import Image from "next/image";
 const REACT_APP_GATEWAY_URL = process.env.NEXT_PUBLIC_GATEWAY_URL;
 
 interface ReviewCardProps {
@@ -51,7 +49,7 @@ const MyVpnCardDedicated: React.FC<ReviewCardProps> = ({
   metaData,
   MyReviews = false,
   onReviewDeleted,
-  onChildValue
+  onChildValue,
 }) => {
   const [showDescription, setShowDescription] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
@@ -60,17 +58,12 @@ const MyVpnCardDedicated: React.FC<ReviewCardProps> = ({
 
   if (!metaData) {
     return (
-      <div
-        className="flex flex-col items-center justify-center w-full max-w-sm mx-auto"
-      >
+      <div className="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
         <div
           className="w-full h-72 p-5 bg-center bg-cover"
           style={{ display: "flex", alignItems: "center" }}
         >
-          <div
-            className="animate-spin rounded-full h-32 w-32 mx-auto border-t-2 border-b-2 border-green-200"
-           
-          ></div>
+          <div className="animate-spin rounded-full h-32 w-32 mx-auto border-t-2 border-b-2 border-green-200"></div>
         </div>
       </div>
     );
@@ -96,134 +89,146 @@ const MyVpnCardDedicated: React.FC<ReviewCardProps> = ({
     const auth = Cookies.get("sotreus_token");
 
     const jsonData = {
-      "vpnId":id
-    }
+      vpnId: id,
+    };
 
     try {
-      const response = await fetch(`${REACT_APP_GATEWAY_URL}api/v1.0/vpn?id=${metaData.name}`, {
-        method: 'DELETE',
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${auth}`,
-        },
-        body: JSON.stringify(jsonData),
-      });
+      const response = await fetch(
+        `${REACT_APP_GATEWAY_URL}api/v1.0/vpn?id=${metaData.name}`,
+        {
+          method: "DELETE",
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${auth}`,
+          },
+          body: JSON.stringify(jsonData),
+        }
+      );
 
       if (response.status === 200) {
-        console.log("success")
+        console.log("success");
         setdelvpn(false);
         onChildValue(`refreshdataafterdelete${metaData.name}`);
       } else {
       }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <div
-      className="w-full px-10"
-      
-    >
-      <div
-        className="w-full h-full py-4 border-t border-gray-500"
-      >
-                <div className="w-full flex justify-between">
-                  <h3
-                    className="text-2xl leading-12 mb-2 text-white w-1/6"
-                    
-                  >
-                    <div className="flex text-lg">
-                      <div>{metaData.name}</div>
-                    </div>
-                  </h3>
+    <div className="w-full px-10">
+      <div className="w-full h-full py-4 border-t border-gray-500">
+        <div className="w-full flex justify-between">
+          <h3 className="text-2xl leading-12 mb-2 text-white w-1/6">
+            <div className="flex text-lg">
+              <div>{metaData.name}</div>
+            </div>
+          </h3>
 
-                  <div className="flex text-lg w-1/6 text-white">
-                      <div>{metaData.region}</div>
-                    </div>
+          <div className="flex text-lg w-1/6 text-white">
+            <div>{metaData.region}</div>
+          </div>
 
-                  <div className="lg:flex md:flex justify-between w-1/6">
-                  <div
-                    
-                  > 
-                    <button className="text-lg rounded-lg text-white">
-                       <a href={`https://${metaData.VpnEndpoint}`} target="_blank" rel="noreferrer" style={color2}>
-                       Link</a>
-                    </button>    
-                  </div>
-              </div>
-                  
-              <button className="text-lg rounded-lg text-white flex w-1/6">
-                       <a href={`https://${metaData.firewallEndpoint}`} target="_blank" rel="noreferrer" style={color2}>
-                          Link</a>
-                    </button> 
+          <div className="lg:flex md:flex justify-between w-1/6">
+            <div>
+              <button className="text-lg rounded-lg text-white">
+                <a
+                  href={`https://${
+                    metaData.VpnEndpoint
+                  }/#/auth?token=${Cookies.get("sotreus_token")}`}
+                  target="_blank"
+                  rel="noreferrer"
+                  style={color2}
+                >
+                  Link
+                </a>
+              </button>
+            </div>
+          </div>
 
-                  <div className="text-white text-lg w-1/6">
-                    <p
-                     
-                      className="flex-col"
-                    >
-                      <Image src={eye} alt="" onClick={togglePasswordVisibility} className="h-5 w-5 mt-1 cursor-pointer"/>
+          <button className="text-lg rounded-lg text-white flex w-1/6">
+            <a
+              href={`https://${metaData.firewallEndpoint}`}
+              target="_blank"
+              rel="noreferrer"
+              style={color2}
+            >
+              Link
+            </a>
+          </button>
 
-                      <div className="">
-                      {showPassword ? (
-
-                        <div className="flex cursor-pointer" onClick={() => {
-                          navigator.clipboard.writeText(
-                            metaData? metaData.password : ''
-                          );
-                        }}>
-                          {metaData.password}
-                          <FaCopy style={{color:'#75E2FF'}} className="ml-2 mt-1"/>
-                          </div>
-                      ) : (
-                        <span></span>
-                      )}
-                      </div>
-                    </p>
-                    
-                  </div>
-
-                  <div className="lg:flex md:flex justify-between w-1/8">
-                  <div
-                    
-                  > 
-                    <button className="text-lg rounded-lg pr-1 text-white" 
-                    onClick={() => setdelvpn(true)}>
-                    {/* onClick={() => deletevpn(metaData.name)}>   */}
-                    <Image src={dlt} alt="info" className="w-4 h-4"/>
-                    </button>    
-                  </div>
-              </div>
-
-                </div>
-                </div>
-                {loading && (
-        <div
-        style={{ backgroundColor: "#0E1414D9" }}
-        className='flex overflow-y-auto overflow-x-hidden fixed inset-0 z-50 justify-center items-center w-full max-h-full'
-        id='popupmodal'
-      >
-        <div className='relative p-4 lg:w-1/5 w-full max-w-2xl max-h-full'>
-          <div className='relative rounded-lg shadow'>
-            <div className='flex justify-center gap-4'>
-              <img
-                className='w-12 animate-spin duration-[3000] h-12'
-                src='/Loadingsotreus.png'
-                alt='Loading icon'
+          <div className="text-white text-lg w-1/6">
+            <p className="flex-col">
+              <Image
+                src={eye}
+                alt=""
+                onClick={togglePasswordVisibility}
+                className="h-5 w-5 mt-1 cursor-pointer"
               />
-  
-              <span className='text-white mt-2'>Loading...</span>
+
+              <div className="">
+                {showPassword ? (
+                  <div
+                    className="flex cursor-pointer"
+                    onClick={() => {
+                      navigator.clipboard.writeText(
+                        metaData ? metaData.password : ""
+                      );
+                    }}
+                  >
+                    {metaData.password}
+                    <FaCopy
+                      style={{ color: "#75E2FF" }}
+                      className="ml-2 mt-1"
+                    />
+                  </div>
+                ) : (
+                  <span></span>
+                )}
+              </div>
+            </p>
+          </div>
+
+          <div className="lg:flex md:flex justify-between w-1/8">
+            <div>
+              <button
+                className="text-lg rounded-lg pr-1 text-white"
+                onClick={() => setdelvpn(true)}
+              >
+                {/* onClick={() => deletevpn(metaData.name)}>   */}
+                <Image src={dlt} alt="info" className="w-4 h-4" />
+              </button>
             </div>
           </div>
         </div>
       </div>
+      {loading && (
+        <div
+          style={{ backgroundColor: "#0E1414D9" }}
+          className="flex overflow-y-auto overflow-x-hidden fixed inset-0 z-50 justify-center items-center w-full max-h-full"
+          id="popupmodal"
+        >
+          <div className="relative p-4 lg:w-1/5 w-full max-w-2xl max-h-full">
+            <div className="relative rounded-lg shadow">
+              <div className="flex justify-center gap-4">
+                <img
+                  className="w-12 animate-spin duration-[3000] h-12"
+                  src="/Loadingsotreus.png"
+                  alt="Loading icon"
+                />
+
+                <span className="text-white mt-2">Loading...</span>
+              </div>
+            </div>
+          </div>
+        </div>
       )}
 
-        {delvpn && (
+      {delvpn && (
         <div
           style={{ backgroundColor: "#0E1414D9" }}
           className="flex overflow-y-auto overflow-x-hidden fixed inset-0 z-10 justify-center items-center w-full max-h-full"
@@ -232,7 +237,10 @@ const MyVpnCardDedicated: React.FC<ReviewCardProps> = ({
           <div className="relative lg:w-1/3 w-full max-w-2xl max-h-full">
             <div
               className="relative rounded-3xl shadow dark:bg-gray-700 p-16 md:p-20"
-              style={{ backgroundColor: "#192424", border: "1px solid #75E2FF"}}
+              style={{
+                backgroundColor: "#192424",
+                border: "1px solid #75E2FF",
+              }}
             >
               <div className="p-4 md:p-5 space-y-4">
                 <p className="text-4xl text-center text-white font-bold">
